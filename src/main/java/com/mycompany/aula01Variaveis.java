5fb9f5b59f3cf0752c9392bc11e83bf0f826f702
luizbeppler <luizbeppler61@gmail.com>
2025-03-26 19:39:58 +0000

    boa tarde

---
+++ src/main/java/com/mycompany/aula01Variaveis.java
@@ -0,0 +1,21 @@
+package com.mycompany;
+
+public class aula01Variaveis {
+
+    public static void main(string[] args) {
+        // variavel lógica
+
+        // tipo primitivo
+        boolean valorLogicoPrimitivo = true;
+
+        //Exibe o valor
+        System.out.printn(valorgicoPrimitivo);
+
+        // Topo abstrato
+        Boolean valorLogicoAbstrato = false;
+
+        //
+        System.out.println("o valor da variável valorLogicoAbstrato é: " + valorLogicoAbstrato);
+
+    }
+}
